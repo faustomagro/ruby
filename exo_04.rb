3425158ae6d1bbc73455ac7e602942015499db3d
faustomagro <77017341+faustomagro@users.noreply.github.com>
2021-01-12 19:59:31 +0100

    Add files via upload

---
+++ exo_04.rb
@@ -0,0 +1,4 @@
+puts "tu as quel age?"
+age = gets.chomp.to_i
+old = 100-age
+puts "ah ouais, ton dans #{old}ans tu auras 100ans lol"
---
+++ exo_06.rb
@@ -0,0 +1,3 @@
+puts "Salut, donne moi un nombre"
+number = gets.chomp.to_i
+puts "Voici ton nombre -1: #{number-1}"
